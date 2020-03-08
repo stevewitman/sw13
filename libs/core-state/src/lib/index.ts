076ba60b116d956b664670a797454d7d986aeb72
sw <sw@sws-MacBook-Pro.local>
2020-03-07 22:13:11 -0700

    Typo fixes

--- libs/core-state/src/lib/index.ts
+++ libs/core-state/src/lib/index.ts
@@ -2,8 +2,8 @@ import { ActionReducerMap } from '@ngrx/store';
 
 import * as fromProjects from  './projects-ngrx/projects.reducer';
 
-export interface AppStata {
-  projects: fromProjects.ProjectState
+export interface AppState {
+  projects: fromProjects.ProjectsState
 }
 
 export const reducers: ActionReducerMap<AppState> ={
